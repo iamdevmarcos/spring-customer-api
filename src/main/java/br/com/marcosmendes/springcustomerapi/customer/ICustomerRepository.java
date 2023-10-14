b46daced93346a0ef63f150403ec6b08926eac1c
Marcos Mendes <marcosdev.me@gmail.com>
2023-10-14 09:20:28 -0300

    Add ICustomerRepository

---
+++ src/main/java/br/com/marcosmendes/springcustomerapi/customer/ICustomerRepository.java
@@ -0,0 +1,9 @@
+package br.com.marcosmendes.springcustomerapi.customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ICustomerRepository extends JpaRepository<CustomerModel, UUID> {
+
+}
