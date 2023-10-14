bfa8a5f24840360ed7c4305c96fa489903511d87
Marcos Mendes <marcosdev.me@gmail.com>
2023-10-14 09:59:51 -0300

    Add CustomerService

---
+++ src/main/java/br/com/marcosmendes/springcustomerapi/customer/services/CustomerService.java
@@ -0,0 +1,31 @@
+package br.com.marcosmendes.springcustomerapi.customer.services;
+
+import br.com.marcosmendes.springcustomerapi.customer.models.CustomerModel;
+import br.com.marcosmendes.springcustomerapi.customer.repositories.ICustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class CustomerService {
+    @Autowired
+    private ICustomerRepository customerRepository;
+
+    public ResponseEntity createCustomer(CustomerModel customer) {
+        var customerAlreadyExists =
+                this.customerRepository.findByCustomerName(customer.getCustomerName());
+
+        if (customerAlreadyExists != null) {
+            var formattedErrorMessage = "Customer called " + customer.getCustomerName() + " already exists!";
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(formattedErrorMessage);
+        }
+
+        customer.setCreatedAt(LocalDateTime.now());
+        var createdCustomer = this.customerRepository.save(customer);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdCustomer);
+    }
+}
