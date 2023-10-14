017bce03ec0f5fa9853c1cfd4111355695d2aa19
Marcos Mendes <marcosdev.me@gmail.com>
2023-10-14 09:01:14 -0300

    chore: add files to repo

---
+++ src/main/java/br/com/marcosmendes/springcustomerapi/SpringCustomerApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcosmendes.springcustomerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCustomerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCustomerApiApplication.class, args);
+	}
+
+}
