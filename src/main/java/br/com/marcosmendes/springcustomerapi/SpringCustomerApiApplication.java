d47bd76a0c11a4a11d11dd69cd29d0f28a5beba6
Marcos Mendes <marcosdev.me@gmail.com>
2023-10-15 14:48:09 -0300

    Add Swagger Documentation

--- src/main/java/br/com/marcosmendes/springcustomerapi/SpringCustomerApiApplication.java
+++ src/main/java/br/com/marcosmendes/springcustomerapi/SpringCustomerApiApplication.java
@@ -1,9 +1,12 @@
 package br.com.marcosmendes.springcustomerapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Spring Customer API", version = "1.0.0", description = "A Spring Boot API for Customer Management."))
 public class SpringCustomerApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/marcosmendes/springcustomerapi/customer/controllers/CustomerController.java
+++ src/main/java/br/com/marcosmendes/springcustomerapi/customer/controllers/CustomerController.java
@@ -2,6 +2,12 @@ package br.com.marcosmendes.springcustomerapi.customer.controllers;
 
 import br.com.marcosmendes.springcustomerapi.customer.models.CustomerModel;
 import br.com.marcosmendes.springcustomerapi.customer.services.CustomerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,20 +17,36 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/customer")
+@Tag(name = "spring-customer-api")
 public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
+    @Operation(summary = "Create a client", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ok"),
+            @ApiResponse(responseCode = "400", description = "Customer Already Exists!"),
+            @ApiResponse(responseCode = "400", description = "Customer Document Already Exists!"),
+    })
     @PostMapping
     public ResponseEntity createCustomer(@RequestBody CustomerModel customer) {
         return this.customerService.createCustomer(customer);
     }
 
+    @Operation(summary = "Returns a general list of all customers", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ok")
+    })
     @GetMapping
     public List<CustomerModel> findAllCustomers() {
         return this.customerService.findAllCustomers();
     }
 
+    @Operation(summary = "Returns a specific customer according to id", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ok"),
+            @ApiResponse(responseCode = "400", description = "Customer Not Found!")
+    })
     @GetMapping(value = "/{customerId}")
     public ResponseEntity findCustomerById(@PathVariable UUID customerId) {
         return this.customerService.findByCustomerId(customerId);
